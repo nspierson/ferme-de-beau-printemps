96cdf7db1e5d42e2fde470ce7891b1624f4ee33d
Nicolas Pierson <nicolas.pierson57@gmail.com>
2019-12-24 13:21:17 +0100

    admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+
+  namespace :admin do
+    root to: 'pages#home'
+    resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
 end
