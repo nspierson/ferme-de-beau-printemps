4499fc62e386ec210d2fae52cfc9248ddac5607f
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-01-09 11:38:40 +0100

    index product -> boutique ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_120042) do
+ActiveRecord::Schema.define(version: 2020_01_08_214242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_01_05_120042) do
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
     t.string "sku"
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
