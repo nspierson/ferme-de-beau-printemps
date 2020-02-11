5f52c619c4cace47b547c169ce5f3c96409466d2
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-02-11 11:49:32 +0100

    stripe1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,7 +22,7 @@ class OrdersController < ApplicationController
         currency: 'eur',
         quantity: 1,
       }],
-      success_url: order_path(order.id),
+      success_url: "http://localhost:3000/orders/#{order.id}",
       cancel_url: 'http://localhost:3000/products'
     )
     order.update(checkout_session_id: session.id)
