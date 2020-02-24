a52de8cc19d78843c7ba0b1541316befc3c98dc7
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-02-24 14:02:14 +0100

    webhooks again1

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,5 +8,5 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
-  events.subscribe 'checkout.session.completed', Stripe::EventHandler.new
+  events.subscribe 'checkout.session.completed', Stripe::StripeCheckoutSessionService.new
 end
