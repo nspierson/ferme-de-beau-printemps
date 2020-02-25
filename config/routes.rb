1e8af79a79c19a7145ecc0dbe62436c02f88b877
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-02-25 14:53:24 +0100

    stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   root to: 'pages#home'
-  mount StripeEvent::Engine, at: '/stripe/webhooks'
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
