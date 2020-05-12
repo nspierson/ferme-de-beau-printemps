84d08100a23f8132abea79f1ed4da37a7dfde901
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-05-12 10:15:16 +0200

    ok

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,8 +1,7 @@
 class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
-    order.update(state: 'paid')
-    order.update(shipping_address: event['shipping']['address']['city'])
+    order.update(state: 'paid', shipping_address: event['data']['object']['shipping']['address']['city'])
     UserMailer.order_success(User.find(order.user_id)).deliver_now
   end
 end
