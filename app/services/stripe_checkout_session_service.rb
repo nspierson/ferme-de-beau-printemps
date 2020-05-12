bd014ff5f56e9a614b4cdbc37bbe2ee7d54a08d7
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-05-12 10:43:35 +0200

    test retriving email from stripe

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,10 @@
 class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
-    order.update(state: 'paid', shipping_address: event['data']['object']['shipping']['address']['city'])
+
+    order.update(state: 'paid', shipping_address: Stripe::Customer.retrieve(event['data']['object']['customer']))
     UserMailer.order_success(User.find(order.user_id)).deliver_now
   end
 end
+
+# event['data']['object']['shipping']['address']['city']
