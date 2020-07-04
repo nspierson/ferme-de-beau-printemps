d3f9d56ab0a0c0f7512e9c16a4e272f18c260e23
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-07-04 11:30:40 +0200

    validates email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :email, uniqueness: true
   validates :street_name, presence: true
   validates :street_number, presence: true
   validates :postal_code, presence: true
